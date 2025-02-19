54174a3b722d98578d01d6d3495bfd178faedd8b
JohnZeng <john.zeng@vesync.com>
2025-02-19 10:16:11 +0800

    Up 注释优化

--- src/base/BasicAlgorithm.java
+++ src/base/BasicAlgorithm.java
@@ -16,9 +16,12 @@ public final class BasicAlgorithm {
      * 这种方式直接将 low 和 high 相加，然后除以2来得到中间值。这种方法在处理较小的整数范围时通常是没有问题的，但是如果 low 和 high 非常大，相加的结果可能会导致整数溢出，从而得到错误的中间值。
      * 推荐在实现二分算法时使用第一种方式 int mid = low + (high - low) / 2，因为它更安全，能够避免整数溢出的问题，尤其是在处理大整数范围时。
      *
+     * pos = {1, 2, 2, 6, 6, 6, 6, 8, 10}  target = 6
+     * l = 3   u = 7  （这里指 index）
+     *
      * @param pos    目标数组，前提条件是，当前数组已经是排好序的，且为递增
      * @param target 目标值
-     * @return 返回，大于且最接近当前target的坐标值
+     * @return 返回，大于等于且最接近当前target的坐标值
      */
     public int lowerBound(List<Integer> pos, int target) {
         int low = 0, high = pos.size() - 1;
